2ce415f869e48f062b1817848ee30f6896b81249
Константин Кудинов <kudinovkostyan@yandex.ru>
2025-06-05 21:42:12 +0600

    Add files via upload

---
+++ Laba1/task4/quadratic_test.c
@@ -0,0 +1,71 @@
+#include <assert.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "quadratic.h"
+
+#define DEFAULT_EPSILON 1e-7
+
+bool is_values_equal(long double a, long double b, long double epsilon) {
+  return fabs(a - b) < epsilon;
+}
+
+void test_zero_coefficient_a() {
+  long double *result = find_roots(0, 1, 1);
+  assert(result == NULL);
+}
+
+void test_coefficient_1_0_m1() {
+  long double *result = find_roots(1, 0, -1);
+  long double correct_answer[2] = {-1.0, 1.0};
+  assert(is_values_equal(result[0], correct_answer[0], DEFAULT_EPSILON) &&
+         is_values_equal(correct_answer[1], result[1], DEFAULT_EPSILON));
+}
+
+void test_coefficient_1_0_0() {
+  long double *result = find_roots(1, 0, 0);
+  long double correct_answer[1] = {0};
+  assert(is_values_equal(result[0], correct_answer[0], DEFAULT_EPSILON));
+}
+
+void test_coefficient_1_0_1() {
+  long double *result = find_roots(1, 0, 1);
+  assert(result == NULL);
+}
+
+void test_coefficient_1_0_m1em7() {
+  long double local_epsilon = 1e-4;
+  long double *result = find_roots(1, 0, -1e-7);
+  long double correct_answer[2] = {-3E-4, 3E-4};
+  assert(is_values_equal(result[0], correct_answer[0], local_epsilon) &&
+         is_values_equal(correct_answer[1], result[1], local_epsilon));
+}
+
+void test_coefficient_1_m1e10_m1() {
+  long double local_epsilon = 1e-11;
+  long double *result = find_roots(1, -1E+10, -1);
+  long double correct_answer[2] = {-1E-10, 1E+10};
+  assert(is_values_equal(result[0], correct_answer[0], local_epsilon) &&
+         is_values_equal(correct_answer[1], result[1], local_epsilon));
+}
+
+void test_coefficient_1_0_m1em8() {
+  long double local_epsilon = 1e-7;
+  long double *result = find_roots(1, 0, -1E-8);
+  long double correct_answer[2] = {-1E-4, 1E-4};
+  assert(is_values_equal(result[0], correct_answer[0], local_epsilon) &&
+         is_values_equal(correct_answer[1], result[1], local_epsilon));
+}
+
+int main() {
+  test_zero_coefficient_a();
+  test_coefficient_1_0_m1();
+  test_coefficient_1_0_0();
+  test_coefficient_1_0_1();
+  test_coefficient_1_0_m1em7();
+  test_coefficient_1_m1e10_m1();
+  test_coefficient_1_0_m1em8();
+  return 0;
+}
