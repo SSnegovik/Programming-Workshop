45a97da2c8ef7a130a91e8e039d3b4d068921712
Константин Кудинов <kudinovkostyan@yandex.ru>
2025-06-11 15:45:55 +0600

    Update hashtable.h

--- Laba2/task3/hashtable.h
+++ Laba2/task3/hashtable.h
@@ -9,11 +9,12 @@ typedef struct HashNode {
 typedef struct HashTable {
   HashNode **buckets;
   size_t capacity;
+  size_t element_size;
   void *allocator;
 } HashTable;
 
-void hashtable_init(HashTable *table, size_t capacity, void *allocator);
-void hashtable_insert(HashTable *table, const char *key, void *value);
+void hashtable_init(HashTable *table, size_t capacity, size_t element_size, void *allocator);
+void hashtable_insert(HashTable *table, const char *key, const void *value);
 void *hashtable_get(HashTable *table, const char *key);
 void hashtable_del(HashTable *table, const char *key);
 void hashtable_free(HashTable *table);
